2f69e9ef574dfdc7642b58097e9aa65d6a080efd
Scott Zager <scott.zager@gmail.com>
2025-08-03 20:44:05 -0400

    added firebase config file to deployment

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// This file contains your private Firebase credentials.
+// It is listed in .gitignore and will NOT be committed to GitHub.
+const firebaseConfig = {
+  apiKey: "AIzaSyDCnTBnMVDbsZbgUHNn0og_NbSiW3HTOi8",
+  authDomain: "name-that-yankee.firebaseapp.com",
+  projectId: "name-that-yankee",
+  storageBucket: "name-that-yankee.firebasestorage.app",
+  messagingSenderId: "697807300683",
+  appId: "1:697807300683:web:b7aa230799b795102545ce"
+};
