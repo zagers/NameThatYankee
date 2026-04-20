7e84a721f04869d980d2ec3b163dc053ba64716e
Scott Zager <scott.zager@gmail.com>
2026-04-20 17:48:36 -0400

    seo: add nofollow to quiz links in gallery generator

--- page-generator/html_generator.py
+++ page-generator/html_generator.py
@@ -398,7 +398,7 @@ def generate_gallery_snippet(i, date_str, formatted_date, search_terms):
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="11" cy="11" r="8"/><path d="m21 21-4.3-4.3"/></svg>
                             <span>Reveal</span>
                         </a>
-                        <a href="quiz?date={date_str}" class="action-link quiz-link">
+                        <a href="quiz?date={date_str}" class="action-link quiz-link" rel="nofollow">
                             <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"/><path d="M12 17h.01"/></svg>
                             <span>Quiz</span>
                         </a>
